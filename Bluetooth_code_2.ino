028df769c3b5e0714c342bf6a4e25dd8710e1421
Paul Tebeila <88151620+paultebeila@users.noreply.github.com>
2022-05-05 15:48:29 +0200

    Add files via upload

---
+++ Bluetooth_code_2.ino
@@ -0,0 +1,29 @@
+int data;
+int light = 13;
+
+void setup()
+{
+ pinMode(light, OUTPUT);
+
+Serial.begin(9600); 
+}
+
+void loop()
+{
+ if(Serial.available())
+{
+ //read the data
+   int data = Serial.read();
+   delay(100);
+   if(data=='1')
+   {
+      digitalWrite(light, 1); 
+   }
+   if(data=='1')
+   {
+      digitalWrite(light, 0); 
+   }
+   
+   delay(100);
+} 
+}
